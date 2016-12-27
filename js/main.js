58a73dca8236b313221f4d805c921a232a22af6c
Suhail Singh Bains <suhail.singhyo@gmail.com>
2016-12-27 22:25:40 +0530

    Partial Done

--- js/main.js
+++ js/main.js
@@ -1,43 +1,21 @@
-function toggle_visibility(id, view) {
-       var e = document.getElementById(id);
-       if(view == 'hide')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
-   }
-
-function show_more_menu(value) {
-  var x = document.getElementById("first_name2");
-  x.value = value;
-  myFunction();
+var i=0;
+function XYZ(){
+  var settings = {
+  "id": i,
+  "title" : "title",
+  "body" : "body",
+  "url": "http://jsonplaceholder.typicode.com/posts/",
+  "method": "GET"
+  };
+  $.ajax(settings).done(function (response) {
+  $("#metaInfo").append("ID: " + settings.id);
+  $("#listOfPeople").append(settings.title + settings.body);
+  console.log(response);
+ });
 }
-function isInt(value) {
-  return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
-}
-function myFunction() {
-    var x = document.getElementById("first_name2");
-    var y = document.getElementById("submit_button");
-
-
-    y.disabled = true;
-    if(x.value.length < 1){
-      y.disabled = false;
-      return 1;
-    }
-
-    if(!isInt(x.value) || x.value < 1 || x.value > 100){
-      $("#metaInfo").append("Invalid ID: " + x.value);
-      y.disabled = false;
-      return 1;
-    }
-    x.value = x.value.replace(/^0+/, '');
-    toggle_visibility('loadingMask', 'show');
-    $.ajax({url: "http://jsonplaceholder.typicode.com/posts/" + x.value.trim(), success: function(result){
-        console.log(result);
-	y.disabled = false;
-        toggle_visibility('loadingMask', 'show');
-	        $("#metaInfo").append("ID: " + result.id);
-        $("#listOfPeople").append('<li class="collection-item avatar"> <i class="material-icons circle green">insert_chart</i><span class="title"><b>' +
-              result.title + '</b></span><p style="text-align:left;">' + result.body + ' </p></li>');
-    }});
+function Test(){
+    var delay=100000;
+  for( ; i<=100 ; i++){
+  setTimeout(XYZ(), delay);
+  }
 }
